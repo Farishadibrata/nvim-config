8548940baa751f21a2a50dccefad8dd90095e861
Faris Hadibrata <me@farishadibrata.com>
2024-06-11 10:34:33 +0700

    changes with settings

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -13,12 +13,23 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
     },
   },
-
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
   -- change trouble config
   {
     "folke/trouble.nvim",
